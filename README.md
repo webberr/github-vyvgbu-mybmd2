db4afb4936d51d77e9245913d4dd9f481f5d91db
webberr <webberr@gmail.com>
2019-04-09 11:23:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-vyvgbu-mybmd2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-vyvgbu-mybmd2)
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FullCalendarModule // import the FullCalendar module! will make the FullCalendar component available
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
